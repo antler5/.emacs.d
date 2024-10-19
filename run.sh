df547e079c3c38c743f964c05f32a604a917a763
antlers <antlers@illucid.net>
2024-10-19 10:40:57 -0700

    Resolve stale `emacs-compat` bytecode
    Not sure what in the package-graph is pulling in `emacs-lispy`, which
    has a failing test-case on `emacs-next-minimal`. Whatev's.

--- run.sh
+++ run.sh
@@ -5,7 +5,10 @@ export GUIX_PACKAGE_PATH+="$HOME/.emacs.d/modules"
 
 guix shell \
   -L ~/.emacs.d/modules \
-  emacs-next \
+  --with-input=emacs=emacs-next \
+  --with-input=emacs-minimal=emacs-next-minimal \
+  --without-tests=emacs-lispy \
+  emacs \
   $(~/.emacs.d/extract-packages.scm < ~/.emacs.d/init.el) \
   $(~/.emacs.d/extract-packages.scm < ~/.emacs.d/early-init.el) \
   -- emacs $@
